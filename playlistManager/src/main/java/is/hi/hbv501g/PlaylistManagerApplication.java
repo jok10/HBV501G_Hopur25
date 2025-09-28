e55f735dac7ac373ee688f3cc08377b602520d42
Jok10 <jok10@hi.is>
2025-09-28 17:25:31 +0000

    project beinagrind

---
+++ playlistManager/src/main/java/is/hi/hbv501g/PlaylistManagerApplication.java
@@ -0,0 +1,17 @@
+package is.hi.hbv501g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+public class PlaylistManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaylistManagerApplication.class, args);
+    }
+}
