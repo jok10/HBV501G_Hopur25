747cbf2748911ad09544e0a592d10cef50d6874f
MountResonance <vlr.mgn-fb@protonmail.com>
2025-10-17 01:42:56 +0000

    JPA annotations, getters, setters: Track.java

--- playlistManager/src/main/java/is/hi/hbv501g/domain/Track.java
+++ playlistManager/src/main/java/is/hi/hbv501g/domain/Track.java
@@ -1,11 +1,36 @@
 package is.hi.hbv501g.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tracks")
 public class Track {
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long trackId;
     private String title;
     private String artist;
     private String album;
     private long durationMs;
 
-    // TODO: getters, setters, constructors
+    public Track() {}
+
+    // Getters
+    public Long getTrackId() { return trackId; }
+    public String getTitle() { return title; }
+    public String getArtist() { return artist; }
+    public String getAlbum() { return album; }
+    public long getDurationMs() { return durationMs; }
+
+    // Setters
+    public void setTrackId(Long trackId) { this.trackId = trackId; }
+    public void setTitle(String title) { this.title = title; }
+    public void setArtist(String artist) { this.artist = artist; }
+    public void setAlbum(String album) {this.album = album; }
+    public void setDurationMs(long durationMs) { this.durationMs = durationMs; }
 }
