4afb2afa7b30efb75f516e9f57cd6df947c35d27
MountResonance <vlr.mgn-fb@protonmail.com>
2025-10-17 01:51:00 +0000

    JPA annotations, getters, setters: User.java

--- playlistManager/src/main/java/is/hi/hbv501g/domain/User.java
+++ playlistManager/src/main/java/is/hi/hbv501g/domain/User.java
@@ -1,10 +1,33 @@
 package is.hi.hbv501g.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "users")
 public class User {
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
     private String username;
     private String email;
     private String passwordHash;
 
-    // TODO: getters, setters, constructors
+    public User() {}
+
+    // Getters
+    public Long getUserId() { return userId; }
+    public String getUsername() { return username; }
+    public String getEmail() { return email; }
+    public String getPasswordHash() { return passwordHash; }
+
+    // Setters
+    public void setUserId(Long userId) { this.userId = userId; }
+    public void setUsername(String username) { this.username = username; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
 }
