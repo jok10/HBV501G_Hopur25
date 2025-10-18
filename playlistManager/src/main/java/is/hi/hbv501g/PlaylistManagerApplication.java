9e78fd38fb474edaceadbb28adf58b1cbcd83adb
MountResonance <vlr.mgn-fb@protonmail.com>
2025-10-18 16:59:14 +0000

    more setup and dependencies

--- playlistManager/src/main/java/is/hi/hbv501g/PlaylistManagerApplication.java
+++ playlistManager/src/main/java/is/hi/hbv501g/PlaylistManagerApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 // this is team member Valentin
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication(scanBasePackages = "is.hi.hbv501g")
+@EnableJpaRepositories(basePackages = "is.hi.hbv501g.repository")
+@EntityScan(basePackages = "is.hi.hbv501g.domain")
 public class PlaylistManagerApplication {
 
     public static void main(String[] args) {
